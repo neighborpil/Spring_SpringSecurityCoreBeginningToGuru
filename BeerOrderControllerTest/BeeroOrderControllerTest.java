cabeff6fdc068e57a6da0632bf0181ee1242c87a
Neighborpil <neighborpil@naver.com>
2021-06-28 20:19:47 +0900

    tdd for multi-tenancy security

---
+++ BeerOrderControllerTest/BeeroOrderControllerTest.java
@@ -0,0 +1,169 @@
+package guru.sfg.brewery.web.controllers.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.sfg.brewery.bootstrap.DefaultBreweryLoader;
+import guru.sfg.brewery.domain.Beer;
+import guru.sfg.brewery.domain.Customer;
+import guru.sfg.brewery.repositories.BeerOrderRepository;
+import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.web.controllers.BaseIT;
+import guru.sfg.brewery.web.model.BeerOrderDto;
+import guru.sfg.brewery.web.model.BeerOrderLineDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class BeeroOrderControllerTest extends BaseIT {
+
+    public static final String API_ROOT = "/api/v1/customers";
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    Customer stPeteCustomer;
+    Customer dunedinCustomer;
+    Customer keyWestCustomer;
+    List<Beer> loadedBeers;
+
+    @BeforeEach
+    void setUp() {
+        stPeteCustomer = customerRepository.findAllByCustomerName(DefaultBreweryLoader.ST_PETE_DISTRIBUTING).orElseThrow();
+        stPeteCustomer = customerRepository.findAllByCustomerName(DefaultBreweryLoader.DUNEDIN_DISTRIBUTING).orElseThrow();
+        stPeteCustomer = customerRepository.findAllByCustomerName(DefaultBreweryLoader.KEY_WEST_DISTRIBUTORS).orElseThrow();
+        loadedBeers = beerRepository.findAll();
+    }
+
+
+    @Test
+    void createOrdernotAuth() throws Exception {
+        BeerOrderDto beerOrderDto = buildOrderDto(stPeteCustomer, loadedBeers.get(0).getId());
+
+        mockMvc.perform(post(API_ROOT + stPeteCustomer.getId() + "/orders")
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beerOrderDto)))
+                .andExpect(status().isUnauthorized());
+    }
+
+
+    @WithUserDetails("spring")
+    @Test
+    void createOrderUserAdmin() throws Exception {
+
+        BeerOrderDto beerOrderDto = buildOrderDto(stPeteCustomer, loadedBeers.get(0).getId());
+
+        mockMvc.perform(post(API_ROOT + stPeteCustomer.getId() + "/order")
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beerOrderDto)))
+                .andExpect(status().isCreated());
+    }
+
+    @WithUserDetails(DefaultBreweryLoader.STPETE_USER)
+    @Test
+    void createOrderUserAuthCustomer() throws Exception {
+        BeerOrderDto beerOrderDto = buildOrderDto(stPeteCustomer, loadedBeers.get(0).getId());
+
+        mockMvc.perform(post(API_ROOT + stPeteCustomer.getId() + "/orders")
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beerOrderDto)))
+                .andExpect(status().isCreated());
+    }
+
+    @WithUserDetails(DefaultBreweryLoader.KEYWEST_USER)
+    @Test
+    void createOrderuserNOTAuthCustomer() throws Exception {
+        BeerOrderDto beerOrderDto = buildOrderDto(stPeteCustomer, loadedBeers.get(0).getId());
+
+        mockMvc.perform(post(API_ROOT + stPeteCustomer.getId() + "/orders")
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beerOrderDto)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void listOrdersNotAuth() throws Exception {
+        mockMvc.perform(get(API_ROOT + stPeteCustomer.getId() + "/orders"))
+                .andExpect(status().isOk());
+    }
+
+    @WithUserDetails(value = DefaultBreweryLoader.STPETE_USER)
+    @Test
+    void listOrdersCustomerAuth() throws Exception {
+        mockMvc.perform(get(API_ROOT + stPeteCustomer.getId() + "/orders"))
+                .andExpect(status().isOk());
+    }
+
+    @WithUserDetails(value = DefaultBreweryLoader.DUNEDIN_USER)
+    @Test
+    void listOrderCustomerNOTAuth() throws Exception {
+        mockMvc.perform(get(API_ROOT + stPeteCustomer.getId() + "/orders"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Disabled
+    @Test
+    void pickUpOrderNotAuth() {
+    }
+
+    @Disabled
+    @Test
+    void pickUpOrderNotAdminUser() {
+    }
+
+    @Disabled
+    @Test
+    void pickUpOrderCustomerUserAUTH() {
+    }
+
+    @Disabled
+    @Test
+    void pickUpOrderCustomerUserNOT_AUTH() {
+    }
+
+    private BeerOrderDto buildOrderDto(Customer customer, UUID beerId) {
+
+        List<BeerOrderLineDto> orderLines = Arrays.asList(BeerOrderLineDto.builder()
+                .id(UUID.randomUUID())
+                .beerId(beerId)
+                .orderQuantity(5)
+                .build());
+
+        return BeerOrderDto.builder()
+                .customerId(customer.getId())
+                .customerRef("123")
+                .orderStatusCallbackUrl("http://example.com")
+                .beerOrderLines(orderLines)
+                .build();
+    }
+
+}
